8304ec6606792e78d37c69d85da53e19203a7f56
Ipin <thangtran882110@gmail.com>
2025-02-26 22:18:34 +0700

    Configure Spring security with JWT and CORS support

---
+++ whatsappclone/src/test/java/com/ipin/whatsappclone/WhatsappcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipin.whatsappclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
